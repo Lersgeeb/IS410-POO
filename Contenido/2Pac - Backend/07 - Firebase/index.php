543de46a39ed9d920fa063ec4928e696ba1ce340
Lersgeeb <bangeas.gabriel13@gmail.com>
2020-04-14 19:44:26 -0600

    Firebase

---
+++ Contenido/2Pac - Backend/07 - Firebase/index.php
@@ -0,0 +1,23 @@
+  
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+
+$factory = (new Factory)
+    ->withServiceAccount('./secret/fir-php-test-49dba-6c01d748532f.json')
+    ->withDatabaseUri('https://fir-php-test-49dba.firebaseio.com');;
+
+$database = $factory->createDatabase();
+
+$newPost = $database
+    ->getReference('users')
+    ->push([
+        'firstName'=>'Juan',
+        'lastName'=>'Perez',
+        'birthDate'=>'12/12/2012',
+        'country'=>'Honduras'
+    ]);
+
+?>
